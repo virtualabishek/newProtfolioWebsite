130aadde4f743b1161a8b694f55f8617f8823c8c
virtualabishek <717abishekneupane@gmail.com>
2024-10-09 22:13:59 +0545

    initial commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <div>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">Projects</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+    </div>
+  );
+};
