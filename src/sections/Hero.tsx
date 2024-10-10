8159fc258814e88285384885b92e3af343efce35
virtualabishek <717abishekneupane@gmail.com>
2024-10-10 08:27:55 +0545

    done for now, Created a star

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,6 +2,7 @@ import memojiImage from "@/assets/images/memoji-computer.png";
 import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
 
 export const HeroSection = () => {
   return (
@@ -9,13 +10,17 @@ export const HeroSection = () => {
       <div
         className="absolute inset-0 -z-30 opacity-5"
         style={{
-          backgroundImage: `url(${grainImage.src}))`,
+          backgroundImage: `url(${grainImage.src})`,
         }}
       ></div>
       <div className=" size-[620px] hero-ring "></div>
       <div className=" size-[820px] hero-ring "></div>
       <div className=" size-[1020px] hero-ring "></div>
       <div className=" size-[1220px] hero-ring "></div>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border border-red-500 size-[800px]">
+        <div className="border border-red-500 inline-flex "></div>
+        <StarIcon className="size-28 text-emerald-300" />
+      </div>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
