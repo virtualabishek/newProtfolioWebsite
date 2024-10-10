6ec6dfdabe7243122636cf1d0a53cda2deae9e6c
virtualabishek <717abishekneupane@gmail.com>
2024-10-10 07:07:22 +0545

    Creation of the navbar is going.

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,19 @@
 export const Header = () => {
   return (
-    <div>
-      <nav>
-        <a href="#">Home</a>
-        <a href="#">Projects</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
+    <div className="flex justify-center items-center relative top-3">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full">
+        <a href="#" className="nav-item">
+          Home
+        </a>
+        <a href="#" className="nav-item">
+          Projects
+        </a>
+        <a href="#" className="nav-item">
+          About
+        </a>
+        <a href="#" className="nav-item">
+          Contact
+        </a>
       </nav>
     </div>
   );
