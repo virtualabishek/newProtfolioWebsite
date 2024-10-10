04c1a117efc7995f6839b8e99404196d00aad0ec
virtualabishek <717abishekneupane@gmail.com>
2024-10-10 08:09:13 +0545

    Finished creating a hero design with more UI/UX

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center relative top-3">
-      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full">
+    <div className="flex justify-center items-center fixed top-3 w-full">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
         <a href="#" className="nav-item">
           Home
         </a>
@@ -11,7 +11,10 @@ export const Header = () => {
         <a href="#" className="nav-item">
           About
         </a>
-        <a href="#" className="nav-item">
+        <a
+          href="#"
+          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+        >
           Contact
         </a>
       </nav>
